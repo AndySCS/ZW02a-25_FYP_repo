359eb95c3fad1bc7aeb3fdd7d13ab58c6eaf19d1
AndySCS <cssunaa@connect.ust.hk>
2025-11-11 15:59:08 +0800

    [Script][git] addedd perl script for git commit

---
+++ script/git_commit.pl
@@ -0,0 +1,78 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my %design_type = (
+    1 => "lib",
+    2 => "axi",
+    3 => "mxu",
+    4 => "ram",
+    5 => "top",
+    6 => "ifu",
+    7 => "idu", 
+    8 => "lsu", 
+    9 => "others",
+);
+
+my %verification_type = (
+    1 => "checker",
+    2 => "sequence",
+    3 => "driver",
+    4 => "intf",
+    5 => "tc",
+    6 => "env",
+    7 => "cov", 
+    8 => "others",
+);
+
+my %script_type = (
+    1 => "git",
+    2 => "others",
+);
+
+my %base_type_dict = (
+    1 => "Design",
+    2 => "Verification",
+    3 => "Script",
+);
+
+my %type_connector = (
+    "Design" => \%design_type,
+    "Verification" => \%verification_type,
+    "Script" => \%script_type,
+);
+
+sub show_options{
+
+    while(1) {
+
+        my (%hash) = @_;
+
+        print "\n=== MENU ===\n";
+        for (sort keys %hash) {
+            print "$_. $hash{$_}\n";
+        }
+
+        print "\nChoice: ";
+        chomp(my $choice = <STDIN>);
+
+        if (exists $hash{$choice}) {
+            return $hash{$choice};
+        } else {
+            print "Invalid option!\n";
+        }
+
+    }
+    
+}
+
+my $base_type = show_options(%base_type_dict);
+print("$base_type");
+my $detail_type = show_options(%{ $type_connector{$base_type} });
+print("$detail_type");
+print "\ncommit message: ";
+chomp(my $commit_msg = <STDIN>);
+print("$commit_msg");
+
+system("git commit -m \"[$base_type][$detail_type] $commit_msg\"");
+
