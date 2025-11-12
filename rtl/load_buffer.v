de54abc7b42aa998edf4c39e0cd72d8669a9f39c
leebigbig <cariciptszwai56@gmail.com>
2025-11-12 18:08:04 +0800

    [design][lsu]fix lsu lint
    except for store buffer line 325
    constant problem

--- rtl/load_buffer.v
+++ rtl/load_buffer.v
@@ -54,10 +54,10 @@ module load_buffer(
     //ctrl_sram_input
     input [7:0]  ctrl_sram_rid;
     input [31:0] ctrl_sram_rdata;
-    input [1:0]  ctrl_sram_rresp,
+    input [1:0]  ctrl_sram_rresp;
     input ctrl_sram_rlast;
     input ctrl_sram_rvld;
-    input ctrl_dram_arrdy
+    input ctrl_dram_arrdy;
 
     //to lsu the internal ram (sram store variables)
     //to ram wrapper
@@ -74,8 +74,10 @@ module load_buffer(
     output [7:0]  load_axi_arlen;
     output [2:0]  load_axi_arsize;
     output [1:0]  load_axi_arburst;
+    output load_axi_arstr;
+    output [3:0] load_axi_arnum;
     output load_axi_arvld;
-    output load_axi_str;
+    output load_axi_rrdy;
 
     wire [11:0] ctrl_load_ld_addr_ff;
     wire [11:0] ctrl_load_addr_nxt;
@@ -84,7 +86,11 @@ module load_buffer(
     wire dram_data_load_done;
     wire sram_data_store_sone;
     wire load_buffer_fsm_nxt;
-
+    wire ctrl_load_resp_resent;
+    wire [7:0] ctrl_load_resent_arid;
+    wire [7:0] ctrl_load_arid;
+    wire [7:0] ctrl_load_dram_araddr;
+    
     assign load_sram_type_next = ctrl_load_sram_type;
     assign load_sram_type_en = ctrl_load_vld;
     assign load_sram_type = ctrl_load_vld ? ctrl_load_sram_type : load_sram_type_ff;
@@ -104,26 +110,27 @@ module load_buffer(
     assign load_axi_arburst = 2'b00; //no burst type now pre is 00 first
     assign load_axi_arvld_nxt = ctrl_load_vld;
     //TODO both axi read dun ahve this str variables need add back
-    assign lsu_axi_str = idu_lsu_str;
+    assign lsu_axi_arstr = ctrl_load_str;
+    
     DFFE #(.WIDTH(2))
-    load_sram_type_ff(
+    ff_load_sram_type(
         .clk(clk),
         .rst_n(rst_n),
         .en(load_sram_type_en),
         .d(load_sram_type_next),
         .q(load_sram_type_ff)
-    )
+    );
 
     DFFE #(.WIDTH(12))
-    idu_lsu_ld_st_addr_ff(
+    ff_idu_lsu_ld_st_addr(
         .clk(clk),
         .rst_n(rst_n),
         .en(ctrl_load_vld),
-        .d(ctrl_load_ld_addr)
+        .d(ctrl_load_ld_addr),
         .q(ctrl_load_ld_addr_ff)
     );
 
-    assign ctrl_load_addr_nxt = ctrl_sram_rvld ? ctrl_load_ld_addr + 1'b1 ctrl_load_ld_addr_ff;
+    assign ctrl_load_addr_nxt = ctrl_sram_rvld ? ctrl_load_ld_addr + 1'b1 : ctrl_load_ld_addr_ff;
     assign ctrl_load_addr_en = ctrl_load_vld | ctrl_sram_rvld;
     DFFE #(.WIDTH(31))
     sram_laod_addr(
@@ -142,14 +149,19 @@ module load_buffer(
     assign lsu_sram_ld_addr = ctrl_load_addr_ff;
     assign lsu_sram_ld_din =  ctrl_sram_rdata;
 
-
+    wire axi_read_rlast;
+    wire [7:0] lsu_store_cur;
+    wire [7:0] lsu_store_len;
+    wire load_buffer_vld;
+    wire sram_data_store_done;
+    
     //dram_data_load_done
     assign dram_data_load_done = axi_read_rlast;
 
     //sram_data_store_done
-    assign sran_data_store_done = lsu_store_cur = lsu_store_len;
+    assign sran_data_store_done = lsu_store_cur == lsu_store_len;
 
-    assign load_buffer_fsm_nxt = load-buffer_vld? 2'b00
+    assign load_buffer_fsm_nxt = load_buffer_vld? 2'b00
                                 : dram_data_load_done ? 2'b10
                                 : sram_data_store_done ? 2'b00 : 2'b11;
 
