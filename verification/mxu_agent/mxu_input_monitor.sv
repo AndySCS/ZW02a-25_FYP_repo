27d20d18b46ebf989033a6a0c15574b39e5d13fa
AndySCS <cssunaa@connect.ust.hk>
2025-12-20 17:49:02 +0800

    [Verification][intf] updated input monitor

--- verification/mxu_agent/mxu_input_monitor.sv
+++ verification/mxu_agent/mxu_input_monitor.sv
@@ -42,8 +42,12 @@ task mxu_input_monitor::collect_matrix_in(mxu_tr tr);
     collect data input from lsu to mxu and store in tr
     */
     
-
+    int iram_array [16] = '{default:0}; //position of column in each row
+    int wram_array [16] = '{default:0};
+    bit [15:0][7:0] iram_data_tmp;
+    bit [15:0][7:0] wram_data_tmp;
     bit collect_begin;
+    bit has_vld;
 
     `uvm_info("mxu_input_monitor", "enter collect matrix", UVM_NONE)
 
@@ -51,54 +55,36 @@ task mxu_input_monitor::collect_matrix_in(mxu_tr tr);
         @(posedge mxu_if.clk);
         if(mxu_if.lsu_mxu_vld & mxu_if.mxu_lsu_rdy) break;
     end
-    
+
     `uvm_info("mxu_input_monitor", "leave collect matrix", UVM_NONE)
 
-/*
     while(1)begin
+
         @(posedge mxu_if.clk);
-        if((|mxu_if.lsu_mxu_iram_vld) | (mxu_if.lsu_mxu_wram_vld))begin
-            collect_begin = 1;
+
+        has_vld = 0;
+
+        iram_data_tmp = mxu_if.lsu_mxu_iram_pld;
+        wram_data_tmp = mxu_if.lsu_mxu_wram_pld;
+
+        for(int i = 0;i < 16;i++)begin
+            if(mxu_if.lsu_mxu_iram_vld[i])begin
+                collect_begin = 1;
+                tr.matrix_L[i][iram_array[i]]  = iram_data_tmp[i];
+                iram_array[i]++;
+                has_vld = 1;
+            end
+            if(mxu_if.lsu_mxu_wram_vld[i])begin
+                collect_begin = 1;
+                tr.matrix_L[i][wram_array[i]]  = wram_data_tmp[i];
+                wram_array[i]++;
+                has_vld = 1;
+            end
         end
-        else if(collect_begin)begin
-            break;
-        end
-        else begin
-            continue;
-        end
-        if(mxu_if.lsu_mxu_iram_vld[0]) tr.matrix_L[0].q.pushback(mxu_if.lsu_mxu_iram_pld[7:0]);
-        if(mxu_if.lsu_mxu_iram_vld[1]) tr.matrix_L[1].q.pushback(mxu_if.lsu_mxu_iram_pld[15:8]);
-        if(mxu_if.lsu_mxu_iram_vld[2]) tr.matrix_L[2].q.pushback(mxu_if.lsu_mxu_iram_pld[23:16]);
-        if(mxu_if.lsu_mxu_iram_vld[3]) tr.matrix_L[3].q.pushback(mxu_if.lsu_mxu_iram_pld[31:24]);
-        if(mxu_if.lsu_mxu_iram_vld[4]) tr.matrix_L[4].q.pushback(mxu_if.lsu_mxu_iram_pld[39:32]);
-        if(mxu_if.lsu_mxu_iram_vld[5]) tr.matrix_L[5].q.pushback(mxu_if.lsu_mxu_iram_pld[47:40]);
-        if(mxu_if.lsu_mxu_iram_vld[6]) tr.matrix_L[6].q.pushback(mxu_if.lsu_mxu_iram_pld[55:48]);
-        if(mxu_if.lsu_mxu_iram_vld[7]) tr.matrix_L[7].q.pushback(mxu_if.lsu_mxu_iram_pld[63:56]);
-        if(mxu_if.lsu_mxu_iram_vld[8]) tr.matrix_L[8].q.pushback(mxu_if.lsu_mxu_iram_pld[71:64]);
-        if(mxu_if.lsu_mxu_iram_vld[9]) tr.matrix_L[9].q.pushback(mxu_if.lsu_mxu_iram_pld[79:72]);
-        if(mxu_if.lsu_mxu_iram_vld[10]) tr.matrix_L[10].q.pushback(mxu_if.lsu_mxu_iram_pld[87:80]);
-        if(mxu_if.lsu_mxu_iram_vld[11]) tr.matrix_L[11].q.pushback(mxu_if.lsu_mxu_iram_pld[95:88]);
-        if(mxu_if.lsu_mxu_iram_vld[12]) tr.matrix_L[12].q.pushback(mxu_if.lsu_mxu_iram_pld[103:96]);
-        if(mxu_if.lsu_mxu_iram_vld[13]) tr.matrix_L[13].q.pushback(mxu_if.lsu_mxu_iram_pld[111:104]);
-        if(mxu_if.lsu_mxu_iram_vld[14]) tr.matrix_L[14].q.pushback(mxu_if.lsu_mxu_iram_pld[119:112]);
-        if(mxu_if.lsu_mxu_iram_vld[15]) tr.matrix_L[15].q.pushback(mxu_if.lsu_mxu_iram_pld[127:120]);
-        if(mxu_if.lsu_mxu_wram_vld[0]) tr.matrix_R[0].q.pushback(mxu_if.lsu_mxu_wram_pld[7:0]);
-        if(mxu_if.lsu_mxu_wram_vld[1]) tr.matrix_R[1].q.pushback(mxu_if.lsu_mxu_wram_pld[15:8]);
-        if(mxu_if.lsu_mxu_wram_vld[2]) tr.matrix_R[2].q.pushback(mxu_if.lsu_mxu_wram_pld[23:16]);
-        if(mxu_if.lsu_mxu_wram_vld[3]) tr.matrix_R[3].q.pushback(mxu_if.lsu_mxu_wram_pld[31:24]);
-        if(mxu_if.lsu_mxu_wram_vld[4]) tr.matrix_R[4].q.pushback(mxu_if.lsu_mxu_wram_pld[39:32]);
-        if(mxu_if.lsu_mxu_wram_vld[5]) tr.matrix_R[5].q.pushback(mxu_if.lsu_mxu_wram_pld[47:40]);
-        if(mxu_if.lsu_mxu_wram_vld[6]) tr.matrix_R[6].q.pushback(mxu_if.lsu_mxu_wram_pld[55:48]);
-        if(mxu_if.lsu_mxu_wram_vld[7]) tr.matrix_R[7].q.pushback(mxu_if.lsu_mxu_wram_pld[63:56]);
-        if(mxu_if.lsu_mxu_wram_vld[8]) tr.matrix_R[8].q.pushback(mxu_if.lsu_mxu_wram_pld[71:64]);
-        if(mxu_if.lsu_mxu_wram_vld[9]) tr.matrix_R[9].q.pushback(mxu_if.lsu_mxu_wram_pld[79:72]);
-        if(mxu_if.lsu_mxu_wram_vld[10]) tr.matrix_R[10].q.pushback(mxu_if.lsu_mxu_wram_pld[87:80]);
-        if(mxu_if.lsu_mxu_wram_vld[11]) tr.matrix_R[11].q.pushback(mxu_if.lsu_mxu_wram_pld[95:88]);
-        if(mxu_if.lsu_mxu_wram_vld[12]) tr.matrix_R[12].q.pushback(mxu_if.lsu_mxu_wram_pld[103:96]);
-        if(mxu_if.lsu_mxu_wram_vld[13]) tr.matrix_R[13].q.pushback(mxu_if.lsu_mxu_wram_pld[111:104]);
-        if(mxu_if.lsu_mxu_wram_vld[14]) tr.matrix_R[14].q.pushback(mxu_if.lsu_mxu_wram_pld[119:112]);
-        if(mxu_if.lsu_mxu_wram_vld[15]) tr.matrix_R[15].q.pushback(mxu_if.lsu_mxu_wram_pld[127:120]);
+
+        if(~collect_begin) continue;
+        else if (~has_vld) break;
+
     end      
-*/
 
 endtask     
