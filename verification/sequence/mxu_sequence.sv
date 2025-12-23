36decc91a3be90dee68689d47b4834217b9e55f0
AndySCS <cssunaa@connect.ust.hk>
2025-12-23 16:03:26 +0800

    [Verification][sequence] added cnn sequence

--- verification/sequence/mxu_sequence.sv
+++ verification/sequence/mxu_sequence.sv
@@ -29,4 +29,46 @@ task mxu_seqeunce::send_random();
 endtask
 
 task mxu_seqeunce::send_cnn();
+    int 	 fd; 			// Variable for file descriptor handle
+    int 	 idx;
+    int      matrix_row = 0;
+    string   line;
+    bit      item_doing = 0;
+
+    fd = $fopen ("img.txt", "r");
+
+    while (!$feof(fd)) begin
+        
+        $fgets(line, fd);
+        if(line.len <= 0) break;
+
+        if(!item_doing)begin
+            tr = mxu_tr::type_id::create("tr");
+            start_item(tr);
+            item_doing = 1;
+            tr.matrix_L[0][8:0] = '{-1, -2, -1, 0, 0, 0, 1, 2, 1};
+            tr.matrix_L[1][8:0] = '{-1, 0, 1, -2, 0, 2, -1, 0, 1};
+        end
+
+        $fscanf (line, "%d,%d,%d,%d,%d,%d,%d,%d,%d", 
+                 tr.matrix_R[matrix_row][0], tr.matrix_R[matrix_row][1], tr.matrix_R[matrix_row][2],
+                 tr.matrix_R[matrix_row][3], tr.matrix_R[matrix_row][4], tr.matrix_R[matrix_row][5],
+                 tr.matrix_R[matrix_row][6], tr.matrix_R[matrix_row][7], tr.matrix_R[matrix_row][8]);
+        matrix_row++;
+
+        if(matrix_row == 16)begin
+            matrix_row = 0;
+            finish_item(tr);
+            item_doing = 0;
+        end
+
+    end
+
+    if(item_doing)begin
+        finish_item(tr);
+    end
+
+    // Close this file handle
+    $fclose(fd);
+
 endtask
