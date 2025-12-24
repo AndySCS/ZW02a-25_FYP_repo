a3597117fa811b9a7de058fc32d6b0d46b8c3180
AndySCS <cssunaa@connect.ust.hk>
2025-12-24 16:13:34 +0800

    [Verification][checker]

--- verification/env/mxu_sc.sv
+++ verification/env/mxu_sc.sv
@@ -1,6 +1,7 @@
 class mxu_sc extends uvm_scoreboard;
 
     mxu_tr exp_result_q[$];
+	bit wr_doing = 0;
     uvm_blocking_get_port #(mxu_tr) exp_port;
     uvm_blocking_get_port #(mxu_tr) act_port;
 
@@ -10,7 +11,8 @@ class mxu_sc extends uvm_scoreboard;
 
     extern virtual function void build_phase(uvm_phase phase);
     extern virtual task main_phase(uvm_phase phase);
-    extern virtual function void write_cnn_result();
+    extern virtual function void write_cnn_result(mxu_tr tr);
+    extern virtual function void write_file(string f_name, string wr_line);
 
     `uvm_component_utils(mxu_sc)
 
@@ -47,6 +49,9 @@ task mxu_sc::main_phase(uvm_phase phase);
 				act_tr.print_result();
 				`uvm_error("mxu_sc", "compare failed");
 			end
+			else begin
+				if($test$plusargs("cnn")) write_cnn_result(tmp_tr);
+			end
 		end
 		else begin
 			`uvm_error("mxu_sc", "exp_result_q is empty")
@@ -57,5 +62,44 @@ task mxu_sc::main_phase(uvm_phase phase);
 
 endtask
 
-function void mxu_sc::write_cnn_result();
+function void mxu_sc::write_cnn_result(mxu_tr tr);
+
+	string int16_x_wr_line = "";
+	string int16_y_wr_line = "";
+	string int8_x_wr_line = "";
+	string int8_y_wr_line = "";
+
+	for(int i = 0; i < 16; i++)begin
+		int16_x_wr_line = {int16_x_wr_line, $sformatf("%d", tr.matrix_result_int16[0][i])};
+		int16_y_wr_line = {int16_y_wr_line, $sformatf("%d", tr.matrix_result_int16[1][i])};
+		int8_x_wr_line  = {int8_x_wr_line , $sformatf("%d", tr.matrix_result_int8 [0][i])};
+		int8_y_wr_line  = {int8_y_wr_line , $sformatf("%d", tr.matrix_result_int8 [1][i])};
+		if(i < 15)begin
+			int16_x_wr_line = {int16_x_wr_line, ","};
+			int16_y_wr_line = {int16_y_wr_line, ","};
+			int8_x_wr_line  = {int8_x_wr_line , ","};
+			int8_y_wr_line  = {int8_y_wr_line , ","};
+		end
+	end
+	
+	write_file("int_16x.csv", int16_x_wr_line);
+	write_file("int_16y.csv", int16_y_wr_line);
+	write_file("int_8x.csv" , int8_x_wr_line );
+	write_file("int_8y.csv" , int8_y_wr_line );
+
+	wr_doing = 1;		
+
+endfunction
+
+function void mxu_sc::write_file(string f_name, string wr_line);
+	
+	int fd;
+
+	if(!wr_doing) fd = $fopen(f_name, "w");
+	else fd = $fopen(f_name, "a");
+
+	$fdisplay(wr_line);
+
+	$fclose(fd);
+
 endfunction
