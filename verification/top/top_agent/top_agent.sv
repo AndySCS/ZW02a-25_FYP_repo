b1c2401a8c786d5b7a33ebd22cef3ba63f0d97c5
AndySCS <cssunaa@connect.ust.hk>
2026-01-15 17:18:35 +0800

    [Verification][driver] updated top driver

--- verification/top/top_agent/top_agent.sv
+++ verification/top/top_agent/top_agent.sv
@@ -2,10 +2,6 @@ class top_agent extends uvm_agent;
     
     top_driver top_drv;
     top_sequencer top_sqr;
-    top_input_monitor top_imon; //input monitor
-    top_output_monitor top_omon; //output monitor
-    uvm_analysis_port #(top_tr) iap;
-    uvm_analysis_port #(top_tr) oap;
 
     function new(string name = "top_agent", uvm_component parent);
         super.new(name, parent);
@@ -21,8 +17,6 @@ endclass //className extends superClass
 function void top_agent::build_phase(uvm_phase phase);
     super.build_phase(phase);
     top_drv = top_driver::type_id::create("top_drv", this);
-    top_imon = top_input_monitor::type_id::create("top_imon", this);
-    top_omon = top_output_monitor::type_id::create("top_omon", this);
     top_sqr = top_sequencer::type_id::create("top_sqr", this);
 endfunction
 
--- verification/top/top_agent/top_driver.sv
+++ verification/top/top_agent/top_driver.sv
@@ -13,8 +13,8 @@ class top_driver extends uvm_driver #(top_tr);
     endfunction //new()
     
     extern function void build_phase(uvm_phase phase);
+    extern virtual task reset_phase(uvm_phase phase);
     extern virtual task main_phase(uvm_phase phase);
-    extern virtual task send_matrix(top_tr tr);
     extern virtual function void final_phase(uvm_phase phase);
 
 endclass //className extends superClass
@@ -26,88 +26,31 @@ function void top_driver::build_phase(uvm_phase phase);
     end
 endfunction
 
-task top_driver::main_phase(uvm_phase phase);
-    
-    top_tr tr;
-    //tr = new("tr");
-    top_if.lsu_top_vld = 0;
-    top_if.lsu_top_iram_vld = 0;
-    top_if.lsu_top_wram_vld = 0;
-    top_if.lsu_top_act_vld = 0;
-    @(posedge top_if.rst_n); // wait till rstn is high
+task top_driver::reset_phase(uvm_phase phase);
 
-    while(1) begin
-        seq_item_port.get_next_item(tr);
-        send_cnt++;
-        `uvm_info("top_driver", $sformatf("tr.matrix_Lx = %d", tr.matrix_Lx), UVM_MEDIUM) 
-        `uvm_info("top_driver", $sformatf("tr.matrix_Ly = %d", tr.matrix_Ly), UVM_MEDIUM) 
-        `uvm_info("top_driver", $sformatf("tr.matrix_Rx = %d", tr.matrix_Rx), UVM_MEDIUM) 
-        `uvm_info("top_driver", $sformatf("tr.matrix_Ry = %d", tr.matrix_Ry), UVM_MEDIUM)
-        `uvm_info("top_driver", $sformatf("tr.matrix_L"), UVM_MEDIUM)
-        tr.print_L(); 
-        `uvm_info("top_driver", $sformatf("tr.matrix_R"), UVM_MEDIUM)
-        tr.print_R(); 
-        send_matrix(tr);
-        seq_item_port.item_done();
-    end
+    super.reset_phase(phase);
+    phase.raise_objection(phase);
+    top_if.rst_n <= 0;
+    top_if.start_vld <= 0;
+    repeat ($urandom_range(100, 0)) @posedge(top_if.clk);
+    phase.drop_objection(phase);
         
 endtask
 
-task top_driver::send_matrix(top_tr tr);
-
-    int matrix_sent_row = 0;
-    int cur_row = 0;
-    int iter_cnt = 0;
-    bit[7:0] pop_data;
-    int cycle_cnt = 0;
-    bit send_matrix_needed = 0;
-
-    while(1)begin
-        @(negedge top_if.clk);
-        if(top_if.top_lsu_rdy) begin
-            top_if.lsu_top_vld = 1;
-            top_if.lsu_top_clr = 1;
-            @(negedge top_if.clk);
-            top_if.lsu_top_vld = 0;
-            top_if.lsu_top_clr = 0;
-            @(negedge top_if.clk);
-            break;
-        end
-    end
-
-    `uvm_info("top_driver", "begin sending matrix", UVM_MEDIUM)
-
-    while(1)begin
-        send_matrix_needed = 0;
-        top_if.lsu_top_iram_vld = 0;
-        top_if.lsu_top_wram_vld = 0;
-        top_if.lsu_top_iram_pld = 0;
-        top_if.lsu_top_wram_pld = 0;
-        for(int row = 0; row < tr.matrix_Lx; row++)begin
-            if(cycle_cnt >= row && cycle_cnt < tr.matrix_Ly + row)begin
-                top_if.lsu_top_wram_vld[row] = 1;
-                pop_data = tr.matrix_L[row][cycle_cnt-row];
-                top_if.lsu_top_wram_pld |= {120'b0, pop_data} << row*8;
-                send_matrix_needed = 1;
-            end
-        end
-        for(int col = 0; col < tr.matrix_Rx; col++)begin
-            if(cycle_cnt >= col && cycle_cnt < tr.matrix_Ry + col)begin
-                top_if.lsu_top_iram_vld[col] = 1;
-                pop_data = tr.matrix_R[col][cycle_cnt-col];
-                top_if.lsu_top_iram_pld |= {120'b0, pop_data} << col*8;
-                send_matrix_needed = 1;
-            end
-        end
-        cycle_cnt++;
-        iter_cnt++;
-        if(iter_cnt >= 500) `uvm_error("top_driver", "maxtrix send function have run over 500 times");
-        if(!send_matrix_needed) break;
-        @(negedge top_if.clk);
-    end
+task top_driver::main_phase(uvm_phase phase);
     
-    `uvm_info("top_driver", "end sending matrix", UVM_MEDIUM)
-
+    super.main_phase(phase);
+    while(1) begin
+        seq_item_port.get_next_item(tr);
+        send_cnt++;
+        @(negedge top_if.clk);
+        top_if.start_vld <= 1;
+        top_if.start_addr <= 0;
+        @(negedge top_if.clk);
+        top_if.rst_n <= 0;
+        seq_item_port.item_done();
+    end
+        
 endtask
 
 function void top_driver::final_phase(uvm_phase phase);
