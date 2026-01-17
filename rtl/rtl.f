6c0a0413246af2e96196e72799fdac8bd6636391
leebigbig <87254827+leebigbig@users.noreply.github.com>
2026-01-17 16:51:35 +0800

    [verifictaion][lsu]update mxu rtl init

--- rtl/rtl.f
+++ rtl/rtl.f
@@ -1,10 +1,10 @@
 -f $MINITPU_RTL/lib/lib.f
 $MINITPU_RTL/define.vh
-//$MINITPU_RTL/acc.v
-//$MINITPU_RTL/act_mod.v
-//$MINITPU_RTL/pe.v
+$MINITPU_RTL/acc.v
+$MINITPU_RTL/act_mod.v
+$MINITPU_RTL/pe.v
 //$MINITPU_RTL/multi.v
-//$MINITPU_RTL/mxu.v
+$MINITPU_RTL/mxu.v
 //$MINITPU_RTL/load_buffer.v
 //$MINITPU_RTL/store_buffer.v
 $MINITPU_RTL/lsu.v
