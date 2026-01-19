3cda6002e47cdb14e78e59749aad4d7ce46ef736
AndySCS <cssunaa@connect.ust.hk>
2026-01-19 20:54:07 +0800

    [Verification][driver] updated axi rd driver recv and send

--- verification/top/axi_rd_agent/axi_rd_driver.sv
+++ verification/top/axi_rd_agent/axi_rd_driver.sv
@@ -2,9 +2,11 @@ class axi_rd_driver extends uvm_driver #(axi_rd_tr);
 
     virtual axi_rd_intf axi_rd_if;
 
-    bit [0:783][7:0] img_array;
-    bit [0:43950][7:0] first_layer_weight;
-    bit [0:569][7:0] second_layer_weigh;
+    bit [783:0][7:0] img_array;
+    bit [43959:0][7:0] first_layer_weight;
+    bit [569:0][7:0] second_layer_weigh;
+
+    axi_read_transaction axi_rd_req_q[$];
 
     `uvm_component_utils(axi_rd_driver)
     
@@ -14,10 +16,17 @@ class axi_rd_driver extends uvm_driver #(axi_rd_tr);
     
     extern function void build_phase(uvm_phase phase);
     extern virtual task main_phase(uvm_phase phase);
+    extern virtual task reset_phase(uvm_phase phase);
 
     extern function void read_img();
     extern function void read_layer1();
     extern function void read_layer2();
+
+    extern function void send_axi_read_recv();
+    extern function void send_axi_read_send();
+    extern function void send_axi_read_send_tr(axi_read_transaction axi_tr);
+    extern function void assign_data2bus(axi_read_transaction axi_tr);
+    extern function bit[63:0] get_data(axi_read_transaction axi_tr);
 
 endclass //className extends superClass
 
@@ -28,13 +37,21 @@ function void axi_rd_driver::build_phase(uvm_phase phase);
     end
 endfunction
 
+task axi_rd_driver::reset_phase(uvm_phase phase); 
+    axi_rd_if.RVALID    <= 0;
+    axi_rd_if.ARREADY   <= 1;
+endtask
+
 task axi_rd_driver::main_phase(uvm_phase phase); 
     super.main_phase(phase);
     //init data
     read_img();
     read_layer1();
     read_layer2();
-    
+    fork
+        send_axi_read_recv();
+        send_axi_read_send();
+    join
 endtask
 
 function void axi_rd_driver::read_img();
@@ -81,7 +98,7 @@ function void axi_rd_driver::read_layer1();
         $fclose(fd);
     end
 
-    if(arr_pos != 43960) `uvm_iierror(get_name(), $sformatf("layer 1 read weight cnt is not correct, actual weight cnt = %d", arr_pos));
+    if(arr_pos != 43960) `uvm_error(get_name(), $sformatf("layer 1 read weight cnt is not correct, actual weight cnt = %d", arr_pos));
 
 endfunction
 
@@ -105,6 +122,106 @@ function void axi_rd_driver::read_layer2();
         $fclose(fd);
     end
 
-    if(arr_pos != 560) `uvm_iierror(get_name(), $sformatf("layer 2 read weight cnt is not correct, actual weight cnt = %d", arr_pos));
+    if(arr_pos != 560) `uvm_error(get_name(), $sformatf("layer 2 read weight cnt is not correct, actual weight cnt = %d", arr_pos));
 
+endfunction
+
+function void axi_rd_driver::send_axi_read_recv();
+    axi_read_transaction axi_rd_tr;
+    int repeated_q[$];
+
+    while(1)begin
+        @posedge(axi_rd_if.clk);
+        repeated_q = axi_rd_req_q.find(axi_req.ARID == axi_read_if.ARID);
+        if(repeated_q.size() > 0)begin
+            `uvm_error(get_name(), $sformatf("repeated ARID is received, ARID = %d", axi_read_if.ARID));
+        end        
+
+        if(axi_rd_if.ARVALID & axi_rd_if.ARREADY)begin
+            axi_rd_tr = axi_read_transaction::type_id::create();
+            axi_rd_tr.init_axi_rd_tr(
+                .ARID(axi_read_if.ARID),
+                .ARADDR(axi_read_if.ARADDR),
+                .ARLEN(axi_read_if.ARLEN),
+                .ARSIZE(axi_read_if.ARSIZE),
+                .ARBURST(axi_read_if.ARBURST),
+                .ARREGION(axi_read_if.ARREGION)
+            );
+            axi_rd_req_q.push_back(axi_rd_tr);
+        end        
+        
+        axi_read_if.ARREADY = (axi_rd_req_q.size() == 16);
+
+    end
+endfunction
+
+function void axi_rd_driver::send_axi_read_send();
+    axi_read_transaction axi_rd_tr;
+    bit is_sending;
+
+    while(1)begin
+        @posedge(axi_rd_if.clk);
+        if(is_sending);
+        else if(axi_rd_req_q.size() > 0)begin
+            axi_rd_tr = axi_rd_req_q.pop_front();
+        end
+
+        if(!is_sending);
+        else if(axi_rd_tr.send_timer >0) axi_rd_tr.send_timer--
+        else begin
+            send_axi_read_send_tr(axi_rd_tr);
+            is_sending = axi_rd_tr.ARLEN >= 0;
+        end
+
+    end
+endfunction
+
+function void axi_rd_driver::send_axi_read_send_tr(axi_read_transaction axi_tr);
+    if(axi_rd_if.RREADY & axi_rd_if.RVALID)begin
+        axi_rd_if.RVALID = 0;
+        axi_tr.ARLEN--;
+        axi_tr.send_cnt = $uramdom_range(100);
+        if(axi_tr.ARBURST == `AXI_WR_BURST_INCR)begin
+            axi_tr.ARADDR += (1 << axi_tr.ARSIZE);
+        end
+        if(axi_tr.ARLEN >= 0 && axi_tr.send_cnt == 0) begin
+            assign_data2bus(axi_tr);
+        end
+    end
+    else begin
+        assign_data2bus(axi_tr);
+    end
+endfunction
+
+function void axi_rd_driver::assign_data2bus(axi_read_transaction axi_tr);
+    axi_rd_if.RVALID = 1;
+    axi_rd_if.RID    = axi_tr.ARID;
+    axi_rd_if.RDATA  = get_data(axi_tr);
+    axi_rd_if.RRESP  = 0;
+    axi_rd_if.RLAST  = (axi_tr.ARLEN == 1);
+endfunction
+
+function bit[63:0] axi_rd_driver::get_data(axi_read_transaction axi_tr);
+    bit[7:0][7:0] rdata_tmp;
+    int arsize_convert;
+
+    arsize_convert = 1 << axi_tr.ARSIZE;
+
+    if(axi_tr.ARADDR < 1000)begin
+        for(int i = 0; i < arsize_convert; i++)begin
+            rdata_tmp[i] = img_array[axi_tr.ARADDR+i];
+        end
+    end
+    else if (axi_tr.ARADDR < 45000) begin 
+        for(int i = 0; i < arsize_convert; i++)begin
+            rdata_tmp[i] = first_layer_weight[axi_tr.ARADDR-1000+i];
+        end
+    end
+    else begin
+        for(int i = 0; i < arsize_convert; i++)begin
+            rdata_tmp[i] = second_layer_weight[axi_tr.ARADDR-45000+i];
+        end
+    end
+
+    return rdata_tmp;
 endfunction
--- verification/top/axi_rd_agent/axi_rd_intf.sv
+++ verification/top/axi_rd_agent/axi_rd_intf.sv
@@ -1,4 +1,34 @@
 interface axi_rd_intf(
+    ARID,
+    ARADDR,
+    ARLEN,
+    ARSIZE,
+    ARBURST,
+    ARREGION,
+    ARVALID,
+    ARREADY,
+    RID,
+    RDATA,
+    RRESP,
+    RLAST,
+    RVALID,
+    RREADY,
 );
+
+    input [AWID_WIDTH-1:0] ARID;
+    input [AWADDR_WIDTH-1:0] ARADDR;
+    input [7:0] ARLEN;
+    input [2:0] ARSIZE;
+    input [1:0] ARBURST;
+    input [3:0] ARREGION;
+    input  ARVALID;
+    input ARREADY;
+
+    input [AWID_WIDTH-1:0] RID;
+    input [WDATA_WIDTH-1:0] RDATA;
+    input [1:0] RRESP;
+    input RLAST;
+    input RVALID;
+    input RREADY; 
 
 endinterface
