805bd1ebde24a845ccdff40f67daf5c27cddb451
leebigbig <87254827+leebigbig@users.noreply.github.com>
2026-01-30 01:19:07 +0800

    [verifictaion][lsu]update lsu 128 bit ld logic

--- verification/ut/lsu/lsu_agent/lsu_driver.sv
+++ verification/ut/lsu/lsu_agent/lsu_driver.sv
@@ -71,8 +71,8 @@ task lsu_driver::main_phase(uvm_phase phase);
 	  	harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0] = 32'h7f;
 	  end
 	  if(i == 0)begin	
-		harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0] = 32'b000000010000_00000_000_00001_0010011;
-    		harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'b0_100000_00000_00001_01_000_000_0000000;
+		harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0] = 32'b000000011000_00000_000_00001_0010011;
+    		harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'b0_100000_00000_00001_01_100_000_0000000;
     		//harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'b0_000000_00100_00000_01_000_000_0000000;
 	  	//harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0] = 32'b0001_00000_00000_0001_0001_0001100;
 	  	//harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'b0001_00000_00000_0001_0001_0001100;
@@ -122,7 +122,7 @@ task lsu_driver::tpu_input(lsu_tr tr);
 		while (1)begin
         		@(negedge tpu_if.clk);
 			tpu_if.RID = count_ld;
-			tpu_if.RDATA = count_ld+99;
+			tpu_if.RDATA = count_ld+64'hfedcba9876543210;
 			tpu_if.RRESP = 0;
 			tpu_if.RVALID = 1;
 			if(count_ld >= 14)begin
