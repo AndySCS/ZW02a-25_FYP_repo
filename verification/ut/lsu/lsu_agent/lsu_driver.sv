c0ff9f36a6e7e673f60a5b8b16d2dcf5c037f9fe
leebigbig <87254827+leebigbig@users.noreply.github.com>
2026-02-01 03:21:13 +0800

    [verifictaion][lsu]update lsu store logic

--- verification/ut/lsu/lsu_agent/lsu_driver.sv
+++ verification/ut/lsu/lsu_agent/lsu_driver.sv
@@ -60,13 +60,13 @@ task lsu_driver::main_phase(uvm_phase phase);
     for (int i=0;i<256;i++) begin
      	  harness.u_tpu.u_lsu.oram.mem[i] = 128'hf0e0d0c0b0a09080;
      	  //harness.u_tpu.u_lsu.oram_hi.mem[i] = 128'hf0e0d0c0b0a09080;
-     	  harness.u_tpu.u_lsu.iram.mem[i] = 128'h4321 ;
-      	  harness.u_tpu.u_lsu.wram.mem[i] = 128'h4321 ;
+     	  harness.u_tpu.u_lsu.iram.mem[i] = 128'h08_07_06_05_04_03_02_01;
+      	  harness.u_tpu.u_lsu.wram.mem[i] = 128'h08_07_06_05_04_03_02_01;
 	  //harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i] = 128'h00004037000030370000203700001137;
-	  harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0] = 32'h00000137 + (32'h1<<(i+12));
-	  harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'h00000137 + (32'h2<<(i+12));
-	  harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][95:64] = 32'h00000137 + (32'h3<<(i+12));
-	  harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][127:96] = 32'h00000137 + (32'h4<<(i+12));
+	  harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0] = 32'h00000537 + (32'h1<<(i+12));
+	  harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'h00000537 + (32'h2<<(i+12));
+	  harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][95:64] = 32'h00000537 + (32'h3<<(i+12));
+	  harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][127:96] = 32'h00000537 + (32'h4<<(i+12));
 	  if(i == 20)begin
 	  	harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0] = 32'h7f;
 	  end
@@ -76,15 +76,19 @@ task lsu_driver::main_phase(uvm_phase phase);
 
     		harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][95:64] = 32'b0_000000_00000_00001_01_001_000_0000000;
     		harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][127:96] = 32'b0_000000_00000_00010_01_001_000_0000000;
-
-    		//harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'b0_000000_00000_00010_01_000_000_0000000;
-    		//harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'b0_000000_00100_00000_01_000_000_0000000;
-	  	//harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0] = 32'b0001_00000_00000_0001_0001_0001100;
-	  	//harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'b0001_00000_00000_0001_0001_0001100;
 	  end
-	  if(i == 5)begin
-	  	harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0]  = 32'b0_00_0001_00000_00000_0001_0001_0001100;
-	  	harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'b0_000000_00000_00000_0001_0001_0000001;
+	  if(i == 1)begin
+		harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'b0000_0000_1101_00001_001_00011_0010011;
+	  end
+	  if(i == 2)begin
+	  	harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0]  = 32'b0_00_0001_00000_00000_0011_0100_0001100;
+	  	harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][63:32] = 32'b0_000000_00000_00000_1111_1111_0000001;
+	  	harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][95:64] = 32'b0_000000_00000_00010_0001_1111_0000001;
+	  	harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][127:96] = 32'b0_000000_00000_00011_1111_0001_0000001;
+	  end
+
+	  if(i == 3)begin
+	  	harness.u_tpu.u_ifu.ifu_mem_wrap_256x128.mem[i][31:0] = 32'b1_000000_00000_00011_1000_0101_0000001;
 	  end
     end
     
@@ -132,7 +136,7 @@ task lsu_driver::tpu_input(lsu_tr tr);
 			end
         		@(negedge tpu_if.clk);
 			tpu_if.RID = count_ld;
-			tpu_if.RDATA = count_ld+64'hfedcba9876543210;
+			tpu_if.RDATA = count_ld+64'h08_07_06_05_04_03_02_01;
 			tpu_if.RRESP = 0;
 			tpu_if.RVALID = 1;
 			
