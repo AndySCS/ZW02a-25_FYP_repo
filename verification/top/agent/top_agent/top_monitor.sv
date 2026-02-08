d050bd507892fd5ffcac2dc53e317f8188890aac
leebigbig <87254827+leebigbig@users.noreply.github.com>
2026-02-08 20:46:34 +0800

    [verification][top]update the top_monitor

--- verification/top/agent/top_agent/top_monitor.sv
+++ verification/top/agent/top_agent/top_monitor.sv
@@ -2,6 +2,7 @@ class top_monitor extends uvm_monitor;
 
     virtual top_intf top_if;
     uvm_analysis_port #(model_output_transaction) ap;
+    uvm_analysis_port #(model_output_transaction) rf_ap;
 
     `uvm_component_utils(top_monitor)
     function new(string name = "top_monitor", uvm_component parent = null);
@@ -18,7 +19,8 @@ function void top_monitor::build_phase(uvm_phase phase);
     if(!uvm_config_db#(virtual top_intf)::get(this, "", "top_if", top_if))begin
         `uvm_fatal(get_name(), "top input_monitor fail to get top if")
     end
-    ap = new("ap", this);
+    ap = new("ap", this);	
+    rf_ap = new("rf_ap", this);
 endfunction
 
 task top_monitor::main_phase(uvm_phase phase);
@@ -31,13 +33,29 @@ task top_monitor::main_phase(uvm_phase phase);
 
     while (1) begin 
 
-        while(1)begin
+        //while(1)begin
             @(posedge top_if.clk)
-            if(top_if.start_vld) begin
-                break;
-            end
+        if(top_if.start_vld) begin
+    	  `uvm_info(get_name(), "start_vld", UVM_NONE);
+	  tr = new();
+                //break;
         end
-        
+        //end
+
+        @(posedge top_if.clk)
+	if(harness.u_tpu.u_rf.lsu_rf_wb_vld)begin
+    	   `uvm_info(get_name(), "wb_vld", UVM_NONE);
+	   `uvm_info("wb_addr", $sformatf("wb_addr: %0h", harness.u_tpu.u_rf.lsu_rf_wb_addr), UVM_NONE);
+	   `uvm_info("wb_data", $sformatf("wb_data: %0h", harness.u_tpu.u_rf.lsu_rf_wb_data), UVM_NONE);
+ 	   tr.model_output[harness.u_tpu.u_rf.lsu_rf_wb_addr] = harness.u_tpu.u_rf.lsu_rf_wb_data;
+	   rf_ap.write(tr);
+	    `uvm_info("rf_ap", $sformatf("rf_ap: %0h", rf_ap), UVM_NONE);
+	end
+	else if(harness.u_tpu.u_lsu.alu_lsu_wfi)begin	
+    	   `uvm_info(get_name(), "reach wfi", UVM_NONE);
+	        `uvm_info("rf_ap", $sformatf("rf_ap: %0h", rf_ap), UVM_NONE);
+	   break;
+	end
         ap.write(tr);
 
     end
