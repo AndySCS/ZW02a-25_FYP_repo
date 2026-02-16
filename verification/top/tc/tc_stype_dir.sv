51ed7862594eac886e0f46fe099a7372aa50f0d8
leebigbig <87254827+leebigbig@users.noreply.github.com>
2026-02-16 22:45:29 +0800

    [verification][top]update st dir tc

---
+++ verification/top/tc/tc_stype_dir.sv
@@ -0,0 +1,27 @@
+`ifndef TC_STYPE_DIR_SV
+`define TC_STYPE_DIR_SV
+
+`define tc_name tc_stype_dir
+
+class `tc_name extends tc_base;
+    `uvm_component_utils(`tc_name)
+   	extern function new(string name="tc_stype_dir", uvm_component parent=null);
+   	extern function void build_phase(uvm_phase phase);
+   	extern task run_phase(uvm_phase phase);
+endclass
+
+function `tc_name::new(string name="tc_stype_dir", uvm_component parent=null);
+    super.new(name, parent);
+endfunction
+
+function void `tc_name::build_phase(uvm_phase phase);
+   	super.build_phase(phase);
+endfunction
+
+task `tc_name::run_phase(uvm_phase phase);
+    stype_dir_seq stype_dir_test = stype_dir_seq::type_id::create("stype_dir_test");
+    stype_dir_test.start(null);
+endtask
+
+`undef tc_name
+`endif
