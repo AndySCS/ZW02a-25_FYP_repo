67f932830f9350c17c70393d76edda04834e6e3f
AndySCS <cssunaa@connect.ust.hk>
2026-03-27 15:04:40 +0800

    [Verification][others] update file list

--- verification/top/env/env.f
+++ verification/top/env/env.f
@@ -1,3 +1,6 @@
+$MINITPU_VER/top/env/ram_block.sv
+$MINITPU_VER/top/env/cmd_handler.sv
+$MINITPU_VER/top/env/model_reader.sv
 $MINITPU_VER/top/env/top_sc.sv
 $MINITPU_VER/top/env/top_rm.sv
 $MINITPU_VER/top/env/force_agent.sv
--- verification/top/tr/tr.f
+++ verification/top/tr/tr.f
@@ -1,3 +1,5 @@
+$MINITPU_VER/top/tr/ffn_operator.sv
+$MINITPU_VER/top/tr/file_read_transaction.sv
 $MINITPU_VER/top/tr/axi_transaction.sv
 $MINITPU_VER/top/tr/model_read_transaction.sv
 $MINITPU_VER/top/tr/model_output_transaction.sv
